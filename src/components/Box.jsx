4aab01454d37e7a92767050a182b5c37387d99df
Angela <angelayuqi@gmail.com>
2023-04-26 05:01:29 -0400

    complete function and refine styling

---
+++ src/components/Box.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import AddIcon from '@mui/icons-material/Add';
+import Fab from '@mui/material/Fab';
+import Zoom from '@mui/material/Zoom';
+
+function Box (props) {
+    const [note, setNote] = useState({
+        title: "",
+        content: ""
+    });
+
+    function handleChange(event) {
+        const { name, value } = event.target;
+        setNote(prevNote => ({...prevNote, [name]: value}));
+    }
+
+    function submitNote(event) {
+        props.onAdd(note);
+        setNote({
+            title: "",
+            content: ""
+        });
+        event.preventDefault();
+    }
+
+    return (
+        <div>
+            <form className="create-note">
+                <input
+                    name="title"
+                    onChange={handleChange}
+                    value={note.title}
+                    placeholder="Title"
+                />
+                <textarea
+                    name="content"
+                    onChange={handleChange}
+                    value={note.content}
+                    placeholder="Note"
+                    rows="3"
+                />
+                <Zoom in={true}>
+                <Fab onClick={submitNote}>
+                    <AddIcon />
+                    </Fab>
+                </Zoom>
+            </form>
+        </div>
+    );
+}
+
+export default Box ;
