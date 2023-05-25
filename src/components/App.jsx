a0ec0b3d083c6dea615ad38e5f05d51c40064a7f
Angela <angelayuqi@gmail.com>
2023-05-25 02:10:29 -0400

    Add GraphQL

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,25 +1,57 @@
-import React, {useState} from "react";
+import React, { useState, useEffect } from "react";
+import "@aws-amplify/ui-react/styles.css";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import Box from "./Box";
 import {withAuthenticator} from '@aws-amplify/ui-react';
-import "@aws-amplify/ui-react/styles.css";
+import { API } from "aws-amplify";
+import { listNotes } from "../graphql/queries";
+import {
+    createNote as createNoteMutation,
+    deleteNote as deleteNoteMutation,
+} from "../graphql/mutations";
 
 
 
 function App({signOut}) {
     const [notes, setNotes] = useState([]);
 
-    function addNote(newNote) {
-        setNotes(prevNotes => {
-            return [...prevNotes, newNote];
-        });
+    useEffect(() => {
+        fetchNotes();
+    }, []);
+
+   async function fetchNotes() {
+        const apiData = await API.graphql({ query: listNotes });
+        const notesFromAPI = apiData.data.listNotes.items;
+        setNotes(notesFromAPI);
     }
 
-    function deleteNote(id) {
-        setNotes(prevNotes => {
-            return prevNotes.filter((noteItem, index) => index !== id);
+   async function createNote(event) {
+        event.preventDefault();
+        const form = new FormData(event.target);
+        const data = {
+            name: form.get("name"),
+            description: form.get("description"),
+        };
+
+        // Create the note in the backend
+        await API.graphql({
+            query: createNoteMutation,
+            variables: { input: data },
+        });
+
+        fetchNotes();
+        event.target.reset();
+    }
+
+
+    async function deleteNote({ id }) {
+        const newNotes = notes.filter((note) => note.id !== id);
+        setNotes(newNotes);
+        await API.graphql({
+            query: deleteNoteMutation,
+            variables: { input: { id } },
         });
     }
 
@@ -27,7 +59,7 @@ function App({signOut}) {
         <div>
             <Header />
             <button className="App-signout-button" onClick={signOut}>Sign Out</button>
-            <Box onAdd={addNote} />
+            <Box onAdd={createNote} />
             {notes.map((noteItem, index) => {
                 return (
                     <Note
